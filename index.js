9bec541b4d03b4e6191102a2fb1b34238bf7f451
Afreen Rahman <studentinsha12@gmail.com>
2017-11-17 15:44:43 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function breakOut(array,changeValue,stopValue) {
+for(lets i=0;i<array.length;i++) {
+  if(array[i]===stopValue)
+   break;
+  array[i]=changeValue;
+}
+  return array;
+}
+
+function keepGoing(array,changeValue,skipValue) {
+  for(let i=0;i<array.length;i++) {
+    
+  }
+}
