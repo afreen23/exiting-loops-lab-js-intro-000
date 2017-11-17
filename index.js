0cef59badd4c1f8151e6238d64df1f652eaab9e8
Afreen Rahman <studentinsha12@gmail.com>
2017-11-17 16:01:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function keepGoing(array,changeValue,skipValue) {
 }
 function findBy(array,findFn) {
   for(let i=0;i<array.length;i++) {
+    debugger
     if(findFn(array[i]))
     return array[i];
   }
